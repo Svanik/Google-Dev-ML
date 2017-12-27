e76dcb1c5605435fecad3b7bbdc812cf5bd8f21e
Svanik <34766427+Svanik@users.noreply.github.com>
2017-12-27 17:45:52 -0500

    Optimized Pipeline
    Optimied piepline for the highest % of predictions moved public docstring ontop of imports

--- Machine Learning Pipeline.py
+++ Machine Learning Pipeline.py
@@ -1,14 +1,15 @@
+"""Uses the Iris Dataset splits it up into 2 setes train and test
+    test is 40% of original dataset
+    Then uses Decision Tree, KNN, and Logistic Regression and makes predictions
+    on the teting data and prints out the accuracy Knn and Decision tree are
+    better at this task then logical Regression"""
+
 from sklearn import datasets
 from sklearn import model_selection
 from sklearn import tree
 from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
-"""Uses the Iris Dataset splits it up into 2 setes train and test
-    test is 40% of original dataset
-    Then uses Decision Tree, KNN, and Logistic Regression and makes predictions
-    on the teting data and prints out the accuracy Knn and Decision tree are
-    better at this task then logical Regression"""
 
 iris = datasets.load_iris()
 
@@ -16,7 +17,7 @@ x = iris.data
 y = iris.target
 
 x_train, x_test, y_train, y_test = model_selection.train_test_split(
-    x, y, test_size=0.4, random_state=7)
+    x, y, test_size=0.44, random_state=7)
 
 
 clftree = tree.DecisionTreeClassifier()
